ae1f6b77f9e05e4c112b16205062bc9282edffde
Ladrillo <gabriel@cabrejas.net>
2022-05-11 18:47:07 +0200

    seed

--- backend/data/seeds/03-quizzes.js
+++ backend/data/seeds/03-quizzes.js
@@ -1,9 +1,29 @@
 exports.seed = async function (knex) {
-  await knex('users').insert([
+  await knex('questions').insert([
     {
-      user_username: 'foo',
-      user_email: 'foo@bar.baz',
-      user_password: '$2a$12$oUAkEKugo6tEDtBrYMuztedRR858zReB3CJrAAjkBibSxf8jKw8ke',
+      question_title: 'Bilbo\'s Pocket',
+      question_text: 'What\'s in Bilbo\'s pocket?',
+      question_hint: 'You know...',
+    },
+  ])
+  await knex('options').insert([
+    {
+      option_text: 'Hand',
+      option_remark: 'Nope...',
+      option_is_distractor: true,
+      question_id: 1,
+    },
+    {
+      option_text: 'Nothing',
+      option_remark: 'Nice try...',
+      option_is_distractor: true,
+      question_id: 1,
+    },
+    {
+      option_text: 'The One Ring',
+      option_remark: 'There ya go!',
+      option_is_distractor: false,
+      question_id: 1,
     },
   ])
 }

a10ba32561fccacc982e073b8bfbde9ff61764b5
Ladrillo <gabriel@cabrejas.net>
2022-05-13 15:38:09 +0200

    wip

--- backend/api/user/user-model.js
+++ backend/api/user/user-model.js
@@ -1,7 +1,7 @@
 const db = require('../../data/db-config')
 
 async function insert(user) {
-  const [user_id] = await db('users').insert(user)
+  const [user_id] = await db('users').insert({ ...user, role_id: 2 })
   return db('users').where({ user_id }).first()
 }
 

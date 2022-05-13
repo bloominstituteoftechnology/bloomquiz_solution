28a536762b2b0f667cdf488bf2e28a6bc0c77ee6
Ladrillo <gabriel@cabrejas.net>
2022-05-13 16:48:08 +0200

    reducer

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -5,4 +5,36 @@ function count(state = initialCount, action) {
   return state
 }
 
-export default combineReducers({ count })
+const initialAuthForm = {
+  username: '', password: ''
+}
+function authForm(state = initialAuthForm, action) {
+  return state
+}
+
+const initialQuestionForm = {
+  question_title: '',
+  question_text: '',
+  question_hint: null,
+  options: [
+    {
+      option_text: '',
+      is_distractor: false,
+      remark: null,
+    },
+    {
+      option_text: '',
+      is_distractor: true,
+      remark: null,
+    },
+  ]
+}
+function questionForm(state = initialQuestionForm, action) {
+  return state
+}
+
+export default combineReducers({
+  count,
+  authForm,
+  questionForm,
+})

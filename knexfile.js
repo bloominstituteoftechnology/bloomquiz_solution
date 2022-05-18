1f657fe923d0d1ae001bcda505201b333567e1b9
Ladrillo <gabriel@cabrejas.net>
2022-05-18 12:11:38 +0200

    get rid of postgres

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,3 @@
-const pg = require('pg')
-
-if (process.env.NODE_ENV === 'production') {
-  pg.defaults.ssl = { rejectUnauthorized: false }
-}
-
 const development = {
   client: 'sqlite3',
   connection: { filename: './backend/data/dev.db3' },
@@ -14,11 +8,12 @@ const development = {
 }
 
 const production = {
-  client: 'pg',
-  connection: process.env.DATABASE_URL,
+  client: 'sqlite3',
+  connection: { filename: './backend/data/prod.db3' },
   migrations: { directory: './backend/data/migrations' },
   seeds: { directory: './backend/data/seeds' },
-  pool: { min: 2, max: 10 },
+  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+  useNullAsDefault: true,
 }
 
 module.exports = {

b8f69cb0494d816bc3f125959a4b09b1b3f7bb21
Ladrillo <gabriel@cabrejas.net>
2022-05-20 19:32:10 +0200

    secure admin area

--- frontend/components/Admin.js
+++ frontend/components/Admin.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { connect } from 'react-redux'
 import QuizForm from './QuizForm'
 
-export default function Admin(props) {
+export function Admin(props) {
+  const navigate = useNavigate()
+  const { auth } = props
+
+  useEffect(() => {
+    if (!auth.admin) navigate('/auth')
+  }, [auth.user])
+
   return (
     <QuizForm />
   )
 }
+
+export default connect(st => st)(Admin)

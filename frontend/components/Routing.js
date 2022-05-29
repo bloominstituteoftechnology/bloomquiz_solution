92d138b479225f9b3077bab2a4dd0e0534e64e57
Ladrillo <gabriel@cabrejas.net>
2022-05-29 12:05:14 +0200

    language

--- frontend/components/Routing.js
+++ frontend/components/Routing.js
@@ -38,7 +38,7 @@ export function Routing(props) {
           <NavLink to="/">{is_admin ? "Preview Quiz" : "Test yourself!"}</NavLink>
           {is_user && !is_admin && <NavLink to="/stats">Stats</NavLink>}
           {is_admin && <NavLink to="/admin">Admin</NavLink>}
-          {!is_user && location.pathname !== '/auth' && <NavLink to="/auth">Sign in to see your stats</NavLink>}
+          {!is_user && location.pathname !== '/auth' && <NavLink to="/auth">Sign in to save your progress</NavLink>}
         </nav>
         <Routes>
           <Route path="/" element={<Quiz navigate={navigate} />} />
